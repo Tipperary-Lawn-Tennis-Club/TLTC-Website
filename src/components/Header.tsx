0ff0a7198b5db9d9be9df03b4a754117916b8c4e
Connor Mattless <connor.mattless@ucd.ie>
2025-02-26 12:11:00 +0000

    refactor: Header and Navbar components, update styles, and enhance layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
+import Navbar from "./Navbar";
+import "../styles/global.css";
 import hero_image from "../assets/hero.jpg";
-
 
 const Header = () => {
 	return (
@@ -10,21 +11,17 @@ const Header = () => {
 					backgroundImage: `url(${hero_image.src})`,
 				}}
 			>
+				<Navbar />
 				<div className="hero-overlay"></div>
 				<div className="hero-content text-neutral-content flex-col">
 					<div className="max-w-2xl text-center">
-						<h1 className="mb-5 text-6xl font-bold">
-							Tennis, Community, Excellence –<br /> Welcome to TLTC
-						</h1>
-						<p className="mb-5">
+						<h1 className="mb-5 text-6xl font-bold">Welcome to TLTC</h1>
+						<p className="mb-5 text-lg">
 							The TLTC is a unique tennis club offering a friendly and cosy
 							atmosphere together with fully heated, modern and ecological
 							facilities which are well maintained.
 						</p>
-						<a
-							href="/contact"
-							className="btn bg-primary/60 text-primary-content"
-						>
+						<a href="/contact" className="btn-cta">
 							Become a member
 						</a>
 					</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import logo_tr from "../assets/logo_tr.png";
+import "../styles/global.css";
 
 const Navbar = () => {
 	return (
-		<div className="navbar p-0 min-h-1.5 bg-gray-300 backdrop-blur-3xl text-gray-700 ">
+		<div className="navbar absolute top-0 p-3 min-h-1.5 bg-gray-900 opacity-75 backdrop-blur-3xl text-gray-200 ">
 			<div className="navbar-start">
 				<img src={logo_tr.src} alt="Club logo" className="w-16 h-16" />
-				<a href="/" className="font-semibold px-3 text-base-content text-sm">
+				<a href="/" className=" px-3 opacity-100 font-bold text-lg">
 					Tipperary Lawn Tennis Club{" "}
 				</a>
 			</div>
@@ -81,8 +82,8 @@ const Navbar = () => {
 					</ul>
 				</div>
 			</div>
-			<div className="navbar-end hidden lg:flex lg:items-center ">
-				<ul className="menu menu-horizontal gap-1 px-1">
+			<div className="navbar-end hidden lg:flex lg:items-center">
+				<ul className="menu menu-horizontal gap-6 font-bold text-lg px-1">
 					<li>
 						<a className="active:bg-primary active:text-primary-content">
 							Club
@@ -96,11 +97,11 @@ const Navbar = () => {
 							News
 						</a>
 					</li>
-					<li>
+					{/* <li>
 						<a className="active:bg-primary active:text-primary-content">
 							Diary
 						</a>
-					</li>
+					</li> */}
 					<li>
 						<a
 							href="/contact"
@@ -115,12 +116,12 @@ const Navbar = () => {
 							target="_blank"
 							rel="noopener noreferrer"
 						>
-							Members Login
+							Non-members
 						</a>
 					</li>
 					<li>
 						<a
-							className="btn bg-accent/30 hover:bg-accent/60"
+							className="btn-cta-sm"
 							href="https://www.smartclubcloud.com/"
 							target="_blank"
 							rel="noopener noreferrer"
